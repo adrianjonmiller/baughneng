fde739fb1e5b1b2474be7fb372195d5b2201a008
Adrian Miller <adrianjonmiller@gmail.com>
2014-07-21 15:13:34 -0700

    Added content to home page, made header semi-transparent

--- wp-content/themes/starkers-child/front-page.php
+++ wp-content/themes/starkers-child/front-page.php
@@ -64,6 +64,10 @@
 	<?php endwhile; ?>
 </ul>
 
-
+<div class="homepage-content">
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<?php the_content(); ?>
+<?php endwhile; ?>
+</div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
