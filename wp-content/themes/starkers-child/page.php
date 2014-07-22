046005bf42ce67904ffaa76092ec69dcafb93e1a
Adrian Miller <adrianjonmiller@gmail.com>
2014-07-22 11:52:40 -0700

    Added styling to default page

--- wp-content/themes/starkers-child/page.php
+++ wp-content/themes/starkers-child/page.php
@@ -16,9 +16,13 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
+<h2 class="semi-transparent-bg heading-large"><?php the_title(); ?></h2>
+
+<div class="col-1">
+	<div class="mdoule semi-transparent-bg">
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<h2><?php the_title(); ?></h2>
 <?php the_content(); ?>
 <?php endwhile; ?>
-
+	</div>
+</div>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
